94274e8043d37e227818965271b6a1a7ea543c56
luxtonecoatings-star <luxtonecoatings@gmail.com>
2025-11-06 22:19:23 +0530

    Update Header.tsx

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -48,7 +48,7 @@ function Header() {
   return (
     <>
       <header
-      >
+       className="fixed w-full top-0 z-50 bg-transparent py-2 sm:py-4 transition-all">
         <nav className="container mx-auto px-4 sm:px-6 lg:px-8">
           <div className="flex justify-between items-center">
             {/* Brand */}
